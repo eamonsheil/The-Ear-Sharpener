07af7819e27727ad7ff2d90215519045f2498b4e
Eamon Sheil <Eamondsheil@gmail.com>
2022-06-08 15:41:35 -0400

    deploy

--- server.js
+++ server.js
@@ -26,11 +26,11 @@ app.use('/totals', require("./routes/totalScoresRoutes"));
 
 // Serve frontend
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/client', '/build')))
+    app.use(express.static(path.join(__dirname, 'client/build')))
 
     app.get('*', (req,res) =>
         res.sendFile(
-            path.resolve(__dirname, '/client', '/build', '/index.html')
+            path.resolve(__dirname, 'client', 'build', 'index.html')
         )
     )
 } else {
