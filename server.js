2b3d7de0e39de00f2a9ba2665b0a873e030d4de6
Eamon Sheil <Eamondsheil@gmail.com>
2022-06-08 15:44:51 -0400

    deploy

--- server.js
+++ server.js
@@ -29,8 +29,7 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, 'client/build')))
 
     app.get('*', (req,res) =>
-        res.sendFile(
-            path.resolve(__dirname, 'client', 'build', 'index.html')
+        res.sendFile(join(__dirname, 'client', 'build')
         )
     )
 } else {
