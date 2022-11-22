b541672b3d9df13c147f3544f0f0f761253e8456
Eamon Sheil <Eamondsheil@gmail.com>
2022-11-21 22:51:07 -0500

    type def

---
+++ declarations.d.ts
@@ -0,0 +1,11 @@
+type ChordArr = {
+    major: number[][] | Tone.FrequencyClass<number>[],
+    minor: number[][] | Tone.FrequencyClass<number>[],
+    augmented: number[][] | Tone.FrequencyClass<number>[],
+    diminished_triad: number[][] | Tone.FrequencyClass<number>[],
+    major_7th: number[][] | Tone.FrequencyClass<number>[],
+    dominant_7th: number[][] | Tone.FrequencyClass<number>[],
+    minor_7th: number[][] | Tone.FrequencyClass<number>[],
+    diminished: number[][] | Tone.FrequencyClass<number>[],
+    half_diminished: number[][] | Tone.FrequencyClass<number>[]
+}
