9df50133588b98f56ac879ddb8d58fdfdb95e14d
Eamon Sheil <Eamondsheil@gmail.com>
2022-12-19 22:11:47 -0800

    user welcome msg

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,18 +1,22 @@
 import { Link } from 'react-router-dom';
 import { TrebleClef } from './TrebleClef';
 import "./home.styles.css";
+import { UserContext } from '../../context/user.context';
+import { useContext } from 'react';
 
 export interface IHomeProps {
   placeholder?: undefined;
 }
 
 export function Home() {
+  const userContext = useContext(UserContext);
   return (
     <div className="home-container">
       <div className="flex titleFlag">
         <TrebleClef/>
         <h1 className='appTitle'>The Ear Sharpener</h1>
       </div>
+      <p>Welcome back, {userContext?.user?.name}</p>
       <div className="flex exercise-links-container">
         <div className="flex chord-link">
           <Link to="./chord_practice" className="flex home-link">
