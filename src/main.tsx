18dbc572bc801e16a3037ec8c54afe77dbec0e5c
Eamon Sheil <Eamondsheil@gmail.com>
2023-02-08 20:02:12 -0500

    domain / clientid config not needed for prod

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { Auth0Provider } from '@auth0/auth0-react';
-import config from "./auth_config.json"
+// import config from "./auth_config.json"
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <>
         <Auth0Provider
-            domain={config.domain || process.env.AUTH_DOMAIN as string}
-            clientId={config.cliendId || process.env.AUTH_CLIENTID as string}
+            domain={process.env.AUTH_DOMAIN as string}
+            clientId={process.env.AUTH_CLIENTID as string}
             redirectUri={window.location.origin}
         >
             <App />
