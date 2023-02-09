744abec1e66c142326e39f88ff3ace602d3e1836
Eamon Sheil <Eamondsheil@gmail.com>
2023-02-09 14:39:47 -0500

    vite env vars

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { Auth0Provider } from '@auth0/auth0-react';
-import config from "./auth_config.json"
+// import config from "./auth_config.json"
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <>
         <Auth0Provider
-            domain={import.meta.env.PROD ? config.domain : import.meta.env.VITE_AUTH_DOMAIN as string}
-            clientId={import.meta.env.PROD ? config.cliendId : import.meta.env.VITE_AUTH_CLIENTID as string}
+            domain={import.meta.env.VITE_AUTH_DOMAIN as string}
+            clientId={import.meta.env.VITE_AUTH_CLIENTID as string}
             redirectUri={window.location.origin}
         >
             <App />
